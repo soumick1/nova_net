e7a6428ed80500a817cb8e5beab847fdfa4ae72b
Soumick Sarker <64783562+soumick1@users.noreply.github.com>
2024-12-17 23:29:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='nova_net',
     version='0.1.0',
-    description='NovaNet: A Gated Multi-Scale Segmentation Architecture for Pothole600 Dataset',
-    author='Your Name',
-    author_email='youremail@example.com',
+    description='NovaNet: A Novel Method for Enhanced Pothole Detection',
+    author='Soumick Sarker',
+    author_email='soumicksarker9@gmail.com',
     packages=find_packages(),
     install_requires=[
         'torch',
